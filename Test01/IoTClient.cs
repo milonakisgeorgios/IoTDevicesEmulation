2f01f69c8e99d48036ac366393abe6b9d13cf008
George Milonakis <milonakis@hotmail.com>
2025-06-11 20:10:25 +0300

    Add project files.

---
+++ Test01/IoTClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01
+{
+    internal class IoTClient
+    {
+        Socket? m_csocket;
+        System.Random rnd = new Random();
+        ManualResetEvent m_quitEvent;
+        string serverIP;
+        int serverPort;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serverIP"></param>
+        /// <param name="serverPort"></param>
+        /// <param name="quitEvent"></param>
+        public IoTClient(string serverIP, int serverPort, ManualResetEvent quitEvent)
+        {
+            this.serverIP = serverIP;
+            this.serverPort = serverPort;
+            m_quitEvent = quitEvent;
+        }
+
+        public void Connect()
+        {
+            m_csocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                m_csocket.Connect(new IPEndPoint(IPAddress.Parse(this.serverIP), this.serverPort));
+                Console.WriteLine("Connected to server.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Drop()
+        {
+            if(m_csocket != null)
+            {
+                m_csocket.Close();
+                m_csocket = null;
+            }
+            else
+            {
+                Console.WriteLine("Client is not connected");
+            }
+        }
+
+
+        public void Quit()
+        {
+            if (m_csocket != null)
+            {
+                m_csocket.Close();
+                m_csocket = null;
+            }
+        }
+
+        //// Send message
+        //string message = "Hello, Server!";
+        //byte[] data = Encoding.UTF8.GetBytes(message);
+        //clientSocket.Send(data);
+        //    Console.WriteLine("Sent: {0}", message);
+
+        //    // Receive response
+        //    byte[] buffer = new byte[256];
+        //int bytesRead = clientSocket.Receive(buffer);
+        //string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        //Console.WriteLine("Received: {0}", response);
+
+        //    // Close socket
+        //    clientSocket.Close();
+
+
+
+    }
+}
---
+++ Test01/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Test01
+{
+    internal class Program
+    {
+
+        static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t\t*******************************************");
+            Console.WriteLine("\t\t*        Q -> Quit");
+            Console.WriteLine("\t\t*        C -> Connect");
+            Console.WriteLine("\t\t*        D -> Drop");
+            Console.WriteLine("\t\t*******************************************");
+        }
+        static void Main(string[] args)
+        {
+            var quitEvent = new ManualResetEvent(false);
+            var client = new IoTClient("192.168.1.6", 5122, quitEvent);
+
+            Task.Factory.StartNew(() => {
+
+                while (true)
+                {
+                    PrintMenu();
+                    var input = Console.ReadKey(true).KeyChar.ToString().ToUpperInvariant();
+
+                    if (input == "Q")
+                    {
+                        Console.WriteLine("\nQuit");
+                        client.Quit();
+                        quitEvent.Set();
+                        Thread.Sleep(500);
+                    }
+                    else if (input == "C")
+                    {
+                        Console.WriteLine("\tConnect");
+                        client.Connect();
+                    }
+                    else if (input == "D")
+                    {
+                        Console.WriteLine("\tDrop");
+                        client.Drop();
+                    }
+                }
+
+            });
+
+
+            quitEvent.WaitOne(-1);
+        }
+    }
+}
