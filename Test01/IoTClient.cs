36c4d05a8fa208360db6cd2229525983edfa8114
George Milonakis <milonakis@hotmail.com>
2025-06-21 09:53:21 +0300

    wip

--- Test01/IoTClient.cs
+++ Test01/IoTClient.cs
@@ -2,19 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Test01
 {
-    internal class IoTClient
+    internal class IoTClient : IDisposable
     {
+        int _disposed = 0;// Whether Dispose has been called.
+        Thread? m_thread;
         Socket? m_csocket;
         System.Random rnd = new Random();
         ManualResetEvent m_quitEvent;
+        readonly Object _sync = new object();
+        readonly ManualResetEvent m_stopEvent;
+        readonly byte[] _rcvBuffer;
         string serverIP;
         int serverPort;
+
+
+        public DateTime LastConnectDT { get; protected set; } = DateTime.MinValue;
+        public DateTime LastDisconnectDT { get; protected set; } = DateTime.MinValue;
+
+        public bool IsConnected
+        {
+            get
+            {
+                if (m_csocket != null)
+                {
+                    lock (_sync)
+                    {
+                        if (m_csocket != null)
+                        {
+                            return m_csocket.Connected;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
 
 
         /// <summary>
@@ -27,23 +56,260 @@ namespace Test01
         {
             this.serverIP = serverIP;
             this.serverPort = serverPort;
+
             m_quitEvent = quitEvent;
+
+            _rcvBuffer = new byte[2048];
+
+            m_stopEvent = new ManualResetEvent(false);
         }
+
+
+        #region Dispose
+        /// <summary>
+        /// 
+        /// </summary>
+        ~IoTClient() => Dispose(false);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
+        void Dispose(bool disposing)
+        {
+            if (Interlocked.Exchange(ref _disposed, 1) != 0)
+            {
+                return;
+            }
+
+
+            if (disposing)
+            {
+                //From user code...
+                Console.WriteLine("Dispose(disposing = true)");
+            }
+            else
+            {
+                Console.WriteLine("Dispose(disposing = false)");
+            }
+
+            _CloseAndInitialize();
+
+            _disposed = 1;
+        }
+
+        void ThrowIfDisposed()
+        {
+            if (_disposed != 0)
+            {
+                ThrowObjectDisposedException();
+            }
+
+            void ThrowObjectDisposedException() => throw new ObjectDisposedException(GetType().FullName);
+        }
+        #endregion
+
+
+        void _CloseAndInitialize()
+        {
+            m_stopEvent.Set();
+
+            if (m_csocket != null)
+            {
+                Disconnect();
+                Thread.Sleep(100);
+            }
+
+            if (m_thread?.IsAlive == true)
+            {
+                m_thread?.Join(1000);
+            }
+
+            m_stopEvent.Reset();
+        }
+
 
         public void Connect()
         {
-            m_csocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ThrowIfDisposed();
+            _CloseAndInitialize();
 
             try
             {
-                m_csocket.Connect(new IPEndPoint(IPAddress.Parse(this.serverIP), this.serverPort));
-                Console.WriteLine("Connected to server.");
+                Console.WriteLine($"Try to connect to {this.serverIP}:{this.serverPort}");
+
+                lock (_sync)
+                {
+                    m_csocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    m_csocket.NoDelay = true;
+                    m_csocket.Connect(new IPEndPoint(IPAddress.Parse(this.serverIP), this.serverPort));
+
+                    Console.WriteLine($"Connected to {m_csocket.RemoteEndPoint}");
+
+
+                    //Κραταμε τι ωρα συνδεθηκαμε
+                    LastConnectDT = DateTime.Now;
+
+                    //Και τωρα δημιουργουμε το νημα που διαβαζει συνεχως τα μηνυματα που ερχονται...
+                    m_thread = new Thread(new ThreadStart(this._ReceiveLoop));
+                    m_thread.Name = "_ReceiveLoop";
+                    m_thread.Start();
+                }
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
+
+
+        public void Stop()
+        {
+            Console.WriteLine($"Stop()");
+
+            ThrowIfDisposed();
+
+            try
+            {
+
+                _CloseAndInitialize();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="invokeEvent"></param>
+        /// <param name="errorcode"></param>
+        void Disconnect(bool invokeEvent = false, int errorcode = 0)
+        {
+            if (Thread.CurrentThread.IsThreadPoolThread)
+                Console.WriteLine($"Disconnect() called by ThreadPoolThread (Id = {Thread.CurrentThread.ManagedThreadId}).");
+            else
+                Console.WriteLine($"Disconnect() called by '{Thread.CurrentThread.Name}'.");
+
+            if (m_csocket != null)
+            {
+                lock (_sync)
+                {
+                    if (m_csocket != null)
+                    {
+                        LastDisconnectDT = DateTime.Now;
+
+                        try
+                        {
+                            try
+                            {
+                                /*
+                                 * When using a connection-oriented Socket, always call the 
+                                 * Shutdown method before closing the Socket. This ensures 
+                                 * that all data is sent and received on the connected socket 
+                                 * before it is closed.
+                                 */
+                                m_csocket.Shutdown(SocketShutdown.Both);
+                            }
+                            catch (SocketException ex)
+                            {
+                                Console.WriteLine($"At m_socket.Shutdown(), SocketException with ErrorCode={ex.ErrorCode}, Message='{ex.Message},'!");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"At m_socket.Shutdown(), Exception with Message='{ex.Message}'!");
+                            }
+                            finally
+                            {
+                                m_csocket.Close();
+                            }
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"In Disconnect(), SocketException with ErrorCode={ex.ErrorCode}, Message='{ex.Message},'!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"In Disconnect(), Exception with Message='{ex.Message}'!");
+                        }
+
+
+                        m_csocket = null;
+
+
+                        if (Thread.CurrentThread.IsThreadPoolThread)
+                            Console.WriteLine($"Disconnected..... by ThreadPoolThread (Id = {Thread.CurrentThread.ManagedThreadId}).");
+                        else
+                            Console.WriteLine($"Disconnected..... by '{Thread.CurrentThread.Name}'.");
+                    }
+                }
+            }
+        }
+
+
+
+
+
+        void _ReceiveLoop()
+        {
+            Console.WriteLine("_ReceiveLoop Started");
+
+            try
+            {
+                while (true)
+                {
+                    if (m_stopEvent.WaitOne(0))
+                    {
+                        Console.WriteLine("m_stopEvent isSet!");
+                        break;
+                    }
+
+                    var numOfBytes = m_csocket!.Receive(_rcvBuffer, _rcvBuffer.Length, SocketFlags.None);
+
+                    if (numOfBytes > 0)
+                    {
+                        string response = Encoding.UTF8.GetString(_rcvBuffer, 0, numOfBytes);
+                        Console.WriteLine("Received: {0}", response);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Received 0 bytes!");
+
+                        //break;
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(string.Format("_ReceiveLoop() -> ErrorCode= {0}, Message = {1}", ex.ErrorCode, ex.Message));
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(string.Format("_ReceiveLoop() -> {0}", ex.Message));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("_ReceiveLoop() -> {0}", ex.Message));
+            }
+
+            Disconnect(invokeEvent: true, errorcode: 0);
+            Console.WriteLine("_ReceiveLoop Terminated");
+        }
+
 
         public void Send1()
         {
@@ -59,7 +325,6 @@ namespace Test01
                 Console.WriteLine("Client is not connected");
             }
         }
-
         public void Send2()
         {
             if (m_csocket != null)
@@ -130,43 +395,7 @@ namespace Test01
                 Console.WriteLine("Client is not connected");
             }
         }
-        public void Drop()
-        {
-            if(m_csocket != null)
-            {
-                m_csocket.Close();
-                m_csocket = null;
-            }
-            else
-            {
-                Console.WriteLine("Client is not connected");
-            }
-        }
 
-
-        public void Quit()
-        {
-            if (m_csocket != null)
-            {
-                m_csocket.Close();
-                m_csocket = null;
-            }
-        }
-
-        //// Send message
-        //string message = "Hello, Server!";
-        //byte[] data = Encoding.UTF8.GetBytes(message);
-        //clientSocket.Send(data);
-        //    Console.WriteLine("Sent: {0}", message);
-
-        //    // Receive response
-        //    byte[] buffer = new byte[256];
-        //int bytesRead = clientSocket.Receive(buffer);
-        //string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-        //Console.WriteLine("Received: {0}", response);
-
-        //    // Close socket
-        //    clientSocket.Close();
 
 
 
--- Test01/Program.cs
+++ Test01/Program.cs
@@ -3,18 +3,28 @@
     internal class Program
     {
 
-        static void PrintMenu()
+        static void PrintMenu(IoTClient client)
         {
             Console.WriteLine();
             Console.WriteLine("\t\t*******************************************");
             Console.WriteLine("\t\t*        Q -> Quit");
-            Console.WriteLine("\t\t*        C -> Connect");
-            Console.WriteLine("\t\t*        1 -> Send1");
-            Console.WriteLine("\t\t*        2 -> Send2");
-            Console.WriteLine("\t\t*        3 -> Send3a");
-            Console.WriteLine("\t\t*        e -> Send3b");
-            Console.WriteLine("\t\t*        4 -> Send4");
-            Console.WriteLine("\t\t*        D -> Drop");
+            if(client.IsConnected)
+            {
+                Console.WriteLine("\t\t*        D -> Drop");
+            }
+            else
+            {
+                Console.WriteLine("\t\t*        C -> Connect");
+            }
+
+            if(client.IsConnected)
+            {
+                Console.WriteLine("\t\t*        1 -> Send1");
+                Console.WriteLine("\t\t*        2 -> Send2");
+                Console.WriteLine("\t\t*        3 -> Send3a");
+                Console.WriteLine("\t\t*        e -> Send3b");
+                Console.WriteLine("\t\t*        4 -> Send4");
+            }
             Console.WriteLine("\t\t*******************************************");
         }
         static void Main(string[] args)
@@ -26,50 +36,95 @@
 
                 while (true)
                 {
-                    PrintMenu();
+                    PrintMenu(client);
                     var input = Console.ReadKey(true).KeyChar.ToString().ToUpperInvariant();
 
                     if (input == "Q")
                     {
                         Console.WriteLine("\nQuit");
-                        client.Quit();
+                        if(client.IsConnected)
+                        {
+                            client.Stop();
+                        }
                         quitEvent.Set();
                         Thread.Sleep(500);
                     }
                     else if (input == "C")
                     {
                         Console.WriteLine("\tConnect");
-                        client.Connect();
+                        if(client.IsConnected == false)
+                        {
+                            client.Connect();
+                        }
                     }
                     else if (input == "D")
                     {
-                        Console.WriteLine("\tDrop");
-                        client.Drop();
+                        Console.WriteLine("Disconnect");
+                        if(client.IsConnected == true)
+                        {
+                            client.Stop();
+                        }
                     }
+
                     else if (input == "1")
                     {
-                        Console.WriteLine("\tSend1");
-                        client.Send1();
+                        if(client.IsConnected)
+                        {
+                            Console.WriteLine("\tSend1");
+                            client.Send1();
+                        }
+                        else
+                        {
+                            Console.WriteLine("NOT_CONNECTED");
+                        }
                     }
                     else if (input == "2")
                     {
-                        Console.WriteLine("\tSend2");
-                        client.Send2();
+                        if (client.IsConnected)
+                        {
+                            Console.WriteLine("\tSend2");
+                            client.Send2();
+                        }
+                        else
+                        {
+                            Console.WriteLine("NOT_CONNECTED");
+                        }
                     }
                     else if (input == "3")
                     {
-                        Console.WriteLine("\tSend3a");
-                        client.Send3a();
+                        if (client.IsConnected)
+                        {
+                            Console.WriteLine("\tSend3a");
+                            client.Send3a();
+                        }
+                        else
+                        {
+                            Console.WriteLine("NOT_CONNECTED");
+                        }
                     }
                     else if (input == "e")
                     {
-                        Console.WriteLine("\tSend3b");
-                        client.Send3b();
+                        if (client.IsConnected)
+                        {
+                            Console.WriteLine("\tSend3b");
+                            client.Send3b();
+                        }
+                        else
+                        {
+                            Console.WriteLine("NOT_CONNECTED");
+                        }
                     }
                     else if (input == "4")
                     {
-                        Console.WriteLine("\tSend4");
-                        client.Send4();
+                        if (client.IsConnected)
+                        {
+                            Console.WriteLine("\tSend4");
+                            client.Send4();
+                        }
+                        else
+                        {
+                            Console.WriteLine("NOT_CONNECTED");
+                        }
                     }
                 }
 
